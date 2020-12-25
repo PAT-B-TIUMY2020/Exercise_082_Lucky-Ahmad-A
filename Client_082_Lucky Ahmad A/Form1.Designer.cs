86dbe24a304ba993d87883c2c6f23b48df3f7741
UB8082 <lucky.ahmad.ft18@mail.umy.ac.id>
2020-12-25 20:55:01 +0700

    add bbutton client

--- Client_082_Lucky Ahmad A/Form1.Designer.cs
+++ Client_082_Lucky Ahmad A/Form1.Designer.cs
@@ -37,13 +37,17 @@
             this.textBoxProdi = new System.Windows.Forms.TextBox();
             this.textBoxAngkatan = new System.Windows.Forms.TextBox();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(66, 105);
+            this.label1.Location = new System.Drawing.Point(66, 43);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(32, 17);
             this.label1.TabIndex = 0;
@@ -52,7 +56,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(66, 161);
+            this.label2.Location = new System.Drawing.Point(66, 103);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(45, 17);
             this.label2.TabIndex = 1;
@@ -61,7 +65,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(66, 214);
+            this.label3.Location = new System.Drawing.Point(70, 170);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(41, 17);
             this.label3.TabIndex = 2;
@@ -70,7 +74,7 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(66, 269);
+            this.label4.Location = new System.Drawing.Point(70, 229);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(68, 17);
             this.label4.TabIndex = 3;
@@ -78,7 +82,7 @@
             // 
             // textBoxNIM
             // 
-            this.textBoxNIM.Location = new System.Drawing.Point(184, 105);
+            this.textBoxNIM.Location = new System.Drawing.Point(184, 43);
             this.textBoxNIM.Name = "textBoxNIM";
             this.textBoxNIM.Size = new System.Drawing.Size(100, 22);
             this.textBoxNIM.TabIndex = 4;
@@ -86,21 +90,21 @@
             // 
             // textBoxNama
             // 
-            this.textBoxNama.Location = new System.Drawing.Point(184, 161);
+            this.textBoxNama.Location = new System.Drawing.Point(184, 100);
             this.textBoxNama.Name = "textBoxNama";
             this.textBoxNama.Size = new System.Drawing.Size(100, 22);
             this.textBoxNama.TabIndex = 5;
             // 
             // textBoxProdi
             // 
-            this.textBoxProdi.Location = new System.Drawing.Point(184, 214);
+            this.textBoxProdi.Location = new System.Drawing.Point(184, 165);
             this.textBoxProdi.Name = "textBoxProdi";
             this.textBoxProdi.Size = new System.Drawing.Size(100, 22);
             this.textBoxProdi.TabIndex = 6;
             // 
             // textBoxAngkatan
             // 
-            this.textBoxAngkatan.Location = new System.Drawing.Point(184, 266);
+            this.textBoxAngkatan.Location = new System.Drawing.Point(184, 226);
             this.textBoxAngkatan.Name = "textBoxAngkatan";
             this.textBoxAngkatan.Size = new System.Drawing.Size(100, 22);
             this.textBoxAngkatan.TabIndex = 7;
@@ -115,11 +119,51 @@
             this.dataGridView1.Size = new System.Drawing.Size(710, 150);
             this.dataGridView1.TabIndex = 8;
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(541, 60);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Insert";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(541, 103);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "Search";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(541, 146);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 11;
+            this.button3.Text = "Update";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(541, 190);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 12;
+            this.button4.Text = "Delete";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.textBoxAngkatan);
             this.Controls.Add(this.textBoxProdi);
@@ -148,6 +192,10 @@
         private System.Windows.Forms.TextBox textBoxProdi;
         private System.Windows.Forms.TextBox textBoxAngkatan;
         private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
     }
 }
 
