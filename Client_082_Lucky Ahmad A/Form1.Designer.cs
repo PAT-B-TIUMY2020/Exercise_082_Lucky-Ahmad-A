86eae372960cfa1a45ff9339c1ec5fdb52591f14
UB8082 <lucky.ahmad.ft18@mail.umy.ac.id>
2020-12-25 21:58:04 +0700

    add classData but still error

--- Client_082_Lucky Ahmad A/Form1.Designer.cs
+++ Client_082_Lucky Ahmad A/Form1.Designer.cs
@@ -37,10 +37,13 @@
             this.textBoxProdi = new System.Windows.Forms.TextBox();
             this.textBoxAngkatan = new System.Windows.Forms.TextBox();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
+            this.buttonInsert = new System.Windows.Forms.Button();
+            this.buttonSearch = new System.Windows.Forms.Button();
+            this.buttonUpdate = new System.Windows.Forms.Button();
+            this.buttonDelete = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.buttonJumlah = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -118,52 +121,89 @@
             this.dataGridView1.RowTemplate.Height = 24;
             this.dataGridView1.Size = new System.Drawing.Size(710, 150);
             this.dataGridView1.TabIndex = 8;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
             // 
-            // button1
+            // buttonInsert
             // 
-            this.button1.Location = new System.Drawing.Point(541, 60);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 9;
-            this.button1.Text = "Insert";
-            this.button1.UseVisualStyleBackColor = true;
+            this.buttonInsert.Location = new System.Drawing.Point(541, 60);
+            this.buttonInsert.Name = "buttonInsert";
+            this.buttonInsert.Size = new System.Drawing.Size(75, 23);
+            this.buttonInsert.TabIndex = 9;
+            this.buttonInsert.Text = "Insert";
+            this.buttonInsert.UseVisualStyleBackColor = true;
+            this.buttonInsert.Click += new System.EventHandler(this.button1_Click);
             // 
-            // button2
+            // buttonSearch
             // 
-            this.button2.Location = new System.Drawing.Point(541, 103);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 10;
-            this.button2.Text = "Search";
-            this.button2.UseVisualStyleBackColor = true;
+            this.buttonSearch.Location = new System.Drawing.Point(541, 103);
+            this.buttonSearch.Name = "buttonSearch";
+            this.buttonSearch.Size = new System.Drawing.Size(75, 23);
+            this.buttonSearch.TabIndex = 10;
+            this.buttonSearch.Text = "Search";
+            this.buttonSearch.UseVisualStyleBackColor = true;
+            this.buttonSearch.Click += new System.EventHandler(this.buttonSearch_Click);
             // 
-            // button3
+            // buttonUpdate
             // 
-            this.button3.Location = new System.Drawing.Point(541, 146);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 23);
-            this.button3.TabIndex = 11;
-            this.button3.Text = "Update";
-            this.button3.UseVisualStyleBackColor = true;
+            this.buttonUpdate.Location = new System.Drawing.Point(541, 146);
+            this.buttonUpdate.Name = "buttonUpdate";
+            this.buttonUpdate.Size = new System.Drawing.Size(75, 23);
+            this.buttonUpdate.TabIndex = 11;
+            this.buttonUpdate.Text = "Update";
+            this.buttonUpdate.UseVisualStyleBackColor = true;
+            this.buttonUpdate.Click += new System.EventHandler(this.buttonUpdate_Click);
             // 
-            // button4
+            // buttonDelete
             // 
-            this.button4.Location = new System.Drawing.Point(541, 190);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(75, 23);
-            this.button4.TabIndex = 12;
-            this.button4.Text = "Delete";
-            this.button4.UseVisualStyleBackColor = true;
+            this.buttonDelete.Location = new System.Drawing.Point(541, 190);
+            this.buttonDelete.Name = "buttonDelete";
+            this.buttonDelete.Size = new System.Drawing.Size(75, 23);
+            this.buttonDelete.TabIndex = 12;
+            this.buttonDelete.Text = "Delete";
+            this.buttonDelete.UseVisualStyleBackColor = true;
+            this.buttonDelete.Click += new System.EventHandler(this.buttonDelete_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(70, 274);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(126, 17);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "Jumlah Mahasiswa";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(323, 274);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(46, 17);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "label7";
+            this.label7.Click += new System.EventHandler(this.label7_Click);
+            // 
+            // buttonJumlah
+            // 
+            this.buttonJumlah.Location = new System.Drawing.Point(541, 265);
+            this.buttonJumlah.Name = "buttonJumlah";
+            this.buttonJumlah.Size = new System.Drawing.Size(75, 23);
+            this.buttonJumlah.TabIndex = 16;
+            this.buttonJumlah.Text = "Jumlah";
+            this.buttonJumlah.UseVisualStyleBackColor = true;
+            this.buttonJumlah.Click += new System.EventHandler(this.buttonJumlah_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button4);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.buttonJumlah);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.buttonDelete);
+            this.Controls.Add(this.buttonUpdate);
+            this.Controls.Add(this.buttonSearch);
+            this.Controls.Add(this.buttonInsert);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.textBoxAngkatan);
             this.Controls.Add(this.textBoxProdi);
@@ -175,6 +215,7 @@
             this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -192,10 +233,13 @@
         private System.Windows.Forms.TextBox textBoxProdi;
         private System.Windows.Forms.TextBox textBoxAngkatan;
         private System.Windows.Forms.DataGridView dataGridView1;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button buttonInsert;
+        private System.Windows.Forms.Button buttonSearch;
+        private System.Windows.Forms.Button buttonUpdate;
+        private System.Windows.Forms.Button buttonDelete;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button buttonJumlah;
     }
 }
 
--- Client_082_Lucky Ahmad A/Form1.cs
+++ Client_082_Lucky Ahmad A/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Service_082_Lucky_Ahmad_A;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,14 +13,116 @@ namespace Client_082_Lucky_Ahmad_A
 {
     public partial class Form1 : Form
     {
+        ClassData data = new ClassData();
         public Form1()
         {
             InitializeComponent();
+        }
+        public void TampilData()
+        {
+            var List = data.getAllData();
+            dataGridView1.DataSource = List;
         }
 
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
 
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            string nim = textBoxNIM.Text;
+            string nama = textBoxNama.Text;
+            string prodi = textBoxProdi.Text;
+            string angkatan = textBoxAngkatan.Text;
+            data.insertMahasiswa(nim, nama, prodi, angkatan);
+            Clear();
+            dataGridView1.DataSource = data.getAllData();
+        }
+        private void Clear()
+        {
+            textBoxNIM.Clear();
+            textBoxNama.Clear();
+            textBoxProdi.Clear();
+            textBoxAngkatan.Clear();
+
+            textBoxNIM.Enabled = true;
+
+            buttonInsert.Enabled = true;
+            buttonUpdate.Enabled = false;
+            buttonDelete.Enabled = false;
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = data.getAllData();
+            TampilData();
+
+            buttonDelete.Enabled = false;
+            buttonUpdate.Enabled = false;
+        }
+
+        private void buttonDelete_Click(object sender, EventArgs e)
+        {
+            ClassData classData = new ClassData();
+            classData.deleteMahasiswa(textBoxNIM.Text);
+            Clear();
+            dataGridView1.DataSource = classData.getAllData();
+        }
+
+        private void buttonUpdate_Click(object sender, EventArgs e)
+        {
+            Mahasiswa mhs = new Mahasiswa();
+            mhs.nim = textBoxNIM.Text;
+            mhs.nama = textBoxNama.Text;
+            mhs.prodi = textBoxProdi.Text;
+            mhs.angkatan = textBoxAngkatan.Text;
+
+            ClassData classData = new ClassData();
+            classData.updateDatabase(mhs);
+            Clear();
+            dataGridView1.DataSource = classData.getAllData();
+        }
+
+        private void buttonSearch_Click(object sender, EventArgs e)
+        {
+            if (textBoxNIM.Text != "")
+            {
+                string nim = textBoxNIM.Text;
+                List<Mahasiswa> mhs = new List<Mahasiswa>();
+                mhs.Add(data.search(nim));
+                Clear();
+                dataGridView1.DataSource = mhs;
+            }
+            else
+            {
+                dataGridView1.DataSource = data.getAllData();
+            }
+        }
+
+        private void buttonJumlah_Click(object sender, EventArgs e)
+        {
+            var jmlh = data.Jumlahdata();
+            label7.Text = jmlh;
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            textBoxNIM.Text = Convert.ToString(dataGridView1.Rows[e.RowIndex].Cells[1].Value);
+            textBoxNama.Text = Convert.ToString(dataGridView1.Rows[e.RowIndex].Cells[0].Value);
+            textBoxProdi.Text = Convert.ToString(dataGridView1.Rows[e.RowIndex].Cells[2].Value);
+            textBoxAngkatan.Text = Convert.ToString(dataGridView1.Rows[e.RowIndex].Cells[3].Value);
+
+            buttonUpdate.Enabled = true;
+            buttonDelete.Enabled = true;
+            buttonInsert.Enabled = false;
         }
     }
 }
